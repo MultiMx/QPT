c9fd46f8d98495946065f711baa7517714608ada
Mmx233 <admin@mmxblog.com>
2022-09-15 16:10:01 +0800

    feat: 打印版本

--- controllers/args/main.go
+++ controllers/args/main.go
@@ -1,20 +1,35 @@
 package args
 
 import (
+	"fmt"
+	"github.com/MultiMx/QPT/global"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
 
 func Run() {
-	if len(os.Args) < 3 {
+	if len(os.Args) == 1 {
 		log.Fatalln("参数缺失")
 	}
+	var length int
+	var f func()
 	switch os.Args[1] {
 	case "install":
-		installers()
+		length = 3
+		f = installers
 	case "update":
-		updater()
+		length = 3
+		f = updater
+	case "version":
+		f = func() {
+			fmt.Println(global.VERSION)
+		}
 	default:
 		log.Fatalf("命令 %s 不存在\n", os.Args[1])
 	}
+
+	if len(os.Args) < length {
+		log.Fatalln("参数不足")
+	}
+	f()
 }
